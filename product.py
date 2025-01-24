3a0d145101b20477053e63bdff44fca1d1660bfd
Wilson Tochi Uduma <tochiwilson10@gmail.com>
2025-01-24 22:50:19 +0100

    Add Product class to the project

---
+++ product.py
@@ -0,0 +1,13 @@
+from category import Category
+
+class Product:
+    def __init__(self, name, price, stock, category):
+        if not isinstance(category, Category):
+            raise ValueError("Category must be an instance of the enum Category.")
+        self.name = name
+        self.price = price
+        self.stock = stock
+        self.category = category
+
+    def __str__(self):
+        return f"{self.name} | {self.category} | €{self.price:.2f} | Stock: {self.stock}"
